f1e26706d3a00c699bcc4a5ba1718aa2bab8e0a3
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-05-04 00:03:12 +0200

    Grammar reader (CFG)

---
+++ reader.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var Rule = require('./rule');
+var types = require('./common');
+
+var Literal = types.Literal;
+
+exports.CFG = function ( cfg ) {
+
+    var grammar = { rules: [], lexicon: {} };
+
+    Object.keys(cfg.Rules).forEach(function (NT) {
+        var mother = new Literal(NT);
+        grammar.rules = grammar.rules.concat(cfg.Rules[NT].map(function(terms) {
+            var children = terms.split(' ').map(function(x) {
+                return new Literal(x);
+            });
+            return new Rule(mother, children);
+        }));
+    });
+
+    Object.keys(cfg.Lexicon).forEach(function (NT) {
+        var cat = new Literal(NT);
+        var l = grammar.lexicon;
+        cfg.Lexicon[NT].forEach(function(term) {
+            if (l[term] === undefined) { l[term] = []; }
+            l[term].push(cat);
+        });
+    });
+
+    return grammar;
+};
--- test.parser.js
+++ test.parser.js
@@ -1,24 +1,30 @@
 "use strict";
 
 var util = require('util');
+var fs = require('fs');
+var yaml = require('js-yaml');
 
 var Parser = require('./parser');
-var English = require('./english');
+var Read = require('./reader');
 
-var p = new Parser(English);
+var grammar = Read.CFG(yaml.safeLoad(fs.readFileSync('english.yml')));
+var sentences = fs.readFileSync('sentences.txt', 'utf8').split('\n');
+
+var p = new Parser(grammar);
 
 function test(i) {
-    var sentence = English.tests[i].split(' ');
+    if (sentences[i] === '') { return; }
+    var sentence = sentences[i].split(' ');
     var parse = p.parse(sentence);
     if (!parse[0]) {
-        console.log("Wrong parse for '"+English.tests[i]+"'");
+        console.log("Wrong parse for '"+sentences[i]+"'");
         console.log(util.inspect(p.table, { depth: null, colors: true }));
     } else {
-        console.log("OK '"+English.tests[i]+"'");
+        console.log("OK '"+sentences[i]+"'");
         console.log(parse[0].node+'');
     }
 }
 
-for (var i=0; i<English.tests.length; i++) {
+for (var i=0; i<sentences.length; i++) {
     test(i);
 }
