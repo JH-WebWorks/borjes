36fbf60dab334f4f8b281b6ce7e53360e726f79a
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-05-30 01:32:22 +0200

    Entry point

---
+++ src/index.js
@@ -0,0 +1,9 @@
+exports.formatter = require('./formatter');
+exports.lexicon = require('./lexicon');
+exports.parenthesis = require('./parenthesis');
+exports.parser = require('./parser');
+exports.reader = require('./reader');
+exports.rule = require('./rule');
+exports.tree = require('./tree');
+exports.types = require('./types');
+exports.unify = require('./unify');
