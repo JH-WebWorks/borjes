674d69f07f6744bfc6fb3f4bb6e3a6880e676a08
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-10-17 19:59:18 +0200

    Started 0.2 rewrite

--- Makefile
+++ Makefile
@@ -1,18 +1,4 @@
-PKGNAME:=borjes
-PUBLISH_URL:=garciasevilla.com:/var/www/pkgs/
-
 test:
-	node test/unify.js
-	node test/cfg.js english.cfg.yml sentences_EN.txt
-	node test/cfg.js english.pcfg.yml sentences_EN.txt
-	node test/cfg.js english.lcfg.yml sentences_EN.txt
-	node test/hpsg.js spanish.json sentences_ES.txt
-
-publish:
-	mkdir -p .publish/$(PKGNAME)
-	cp -r lib src package.json .publish/$(PKGNAME)
-	cd .publish && tar -czf $(PKGNAME).tar.gz $(PKGNAME)
-	scp .publish/$(PKGNAME).tar.gz $(PUBLISH_URL)
-	rm -rf .publish
+	babel-node test/unify.js
 
 .PHONY: publish test
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-"use strict";
+let Borjes = {
+    hello_es6() {
+        console.log("Hello modern world");
+    }
+};
 
-exports.formatter = require('./formatter');
-exports.Lexicon = require('./lexicon');
-exports.parenthesis = require('./parenthesis');
-exports.Parser = require('./parser');
-exports.Rule = require('./rule');
-exports.Principle = require('./principle');
-exports.Tree = require('./tree');
-exports.types = require('./types');
-exports.unify = require('./unify');
-
-var grammar = require('./grammar');
-grammar.from_YAML = require('./from_yaml');
-grammar.from_JSON = require('./from_json');
-exports.Grammar = grammar;
+export default Borjes;
