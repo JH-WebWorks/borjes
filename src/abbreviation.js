ff2f97e92a7004514dd2688f2b0f18b942d01369
lisa <lisa.hetzel@jh-webworks.de>
2021-07-01 21:40:57 +0200

    changed name of abbr from principle to abbr

--- src/abbreviation.js
+++ src/abbreviation.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var types = require('./types');
-var unify = require('./unify');
+var types = require("./types");
+var unify = require("./unify");
 
 var Nothing = types.Nothing;
 var World = types.World;
@@ -11,35 +11,34 @@ var World = types.World;
  *
  * @param {Borjes} antecedent
  * @param {Borjes} consequent
- * @param {String} [name="Principle"]
+ * @param {String} [name="Abbreviation"]
  * @param {Object} [events={}] - callbacks to fire during the principle's application.
  * @param {function} events.false_antecedent
  * @param {function} events.fail
  * @param {function} events.success
  * @return {Principle} a new principle.
  */
-function Abbreviation ( fs, name, events ) {
-    /**
-     * A principle has an antecedent, which if matches forces the object to also
-     * match the consequent.
-     *
-     * @typedef Principle
-     * @property {String} borjes - 'principle'
-     * @property {Borjes} antecedent
-     * @property {Borjes} consequent
-     * @property {String} name
-     * @property {Object} on - Callbacks for the rule
-     * @property {function} on.false_antecedent
-     * @property {function} on.fail
-     * @property {function} on.success
-     */
-    return {
-        borjes: 'abbreviation',
-        fstr: fs,
-        name: name || "",
-        on: events || {}
-    };
+function Abbreviation(fs, name, events) {
+  /**
+   * A principle has an antecedent, which if matches forces the object to also
+   * match the consequent.
+   *
+   * @typedef Principle
+   * @property {String} borjes - 'principle'
+   * @property {Borjes} antecedent
+   * @property {Borjes} consequent
+   * @property {String} name
+   * @property {Object} on - Callbacks for the rule
+   * @property {function} on.false_antecedent
+   * @property {function} on.fail
+   * @property {function} on.success
+   */
+  return {
+    borjes: "abbreviation",
+    fstr: fs,
+    name: name || "",
+    on: events || {},
+  };
 }
-
 
 module.exports = Abbreviation;
