192738eabf03b9bf6cd90afae4e12f71277c3e7d
lisa <lisa.hetzel@jh-webworks.de>
2021-07-01 21:53:04 +0200

    added export of abbreviation and declaration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 "use strict";
 
-exports.formatter = require('./formatter');
-exports.Lexicon = require('./lexicon');
-exports.parenthesis = require('./parenthesis');
-exports.Parser = require('./parser');
-exports.Rule = require('./rule');
-exports.Principle = require('./principle');
-exports.Tree = require('./tree');
-exports.types = require('./types');
-exports.unify = require('./unify');
+exports.formatter = require("./formatter");
+exports.Lexicon = require("./lexicon");
+exports.parenthesis = require("./parenthesis");
+exports.Parser = require("./parser");
+exports.Rule = require("./rule");
+exports.Principle = require("./principle");
+exports.Abbreviation = require("./abbreviation");
+exports.Declaration = require("./declaration");
+exports.Tree = require("./tree");
+exports.types = require("./types");
+exports.unify = require("./unify");
 
-var grammar = require('./grammar');
-grammar.from_YAML = require('./from_yaml');
-grammar.from_JSON = require('./from_json');
+var grammar = require("./grammar");
+grammar.from_YAML = require("./from_yaml");
+grammar.from_JSON = require("./from_json");
 exports.Grammar = grammar;
